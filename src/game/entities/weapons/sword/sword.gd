d14729ea613908bfaa240679b33f432f72f3f846
Tom Rushton <tom.rushton@hotmail.co.uk>
2025-07-12 22:13:03 +0100

    Make sword follow mouse

--- src/game/entities/weapons/sword/sword.gd
+++ src/game/entities/weapons/sword/sword.gd
@@ -9,16 +9,7 @@ const ATTACK_DURATION_S : float = 0.25
 @onready var _hurtbox : Area2D = $Hurtbox
 
 var _attack_progress_s : float = -1.0
-var _facing : Vector2 = Vector2.DOWN
-var _render_facing : Vector2 = Vector2.DOWN
-
-
-func _on_owner_direction_changed(_drection: Enums.Direction) -> void:
-	_facing = _owner.get_direction_component().get_angle()
-	
-	if _attack_progress_s == -1.0:
-		_render_facing = _facing
-		_update_position(0)
+var _attack_direction : Vector2
 	
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
@@ -31,7 +22,7 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 
 func _on_fired() -> void:
 	_attack_progress_s = 0.0
-	_render_facing = _facing
+	_attack_direction = _owner.global_position.direction_to(get_global_mouse_position())
 	
 	var percentage_progress : float = _attack_progress_s / ATTACK_DURATION_S
 	_update_position(percentage_progress)
@@ -40,13 +31,16 @@ func _on_fired() -> void:
 
 func _on_ready() -> void:
 	_update_position(0.0)
-	
-	_facing = _owner.get_direction_component().get_angle()
-	_owner.get_direction_component().direction_changed.connect(_on_owner_direction_changed)
 
 
 func _physics_process(delta: float) -> void:
 	if _attack_progress_s < 0.0:
+		var center_offset : Vector2 = Vector2(0, -64) # Horrible hardcoding
+		var mouse_pos : Vector2 = get_global_mouse_position()
+		var mouse_angle : Vector2 = _owner.global_position.direction_to(mouse_pos)
+		
+		global_position = _owner.global_position + center_offset + (mouse_angle * _attack_circle_radius)
+		rotation = mouse_angle.angle() + (TAU / 4)
 		return
 		
 	_attack_progress_s += delta
@@ -56,15 +50,13 @@ func _physics_process(delta: float) -> void:
 	
 	if _attack_progress_s > ATTACK_DURATION_S:
 		_attack_progress_s = -1.0
-		_render_facing = _facing
-		_update_position(0)
 		_hurtbox.monitoring = false
 
 
 func _update_position(percentage_progress: float) -> void:
-	var angle_rads: float = (_sweep_angle_rads * percentage_progress) + (TAU / 8) + _render_facing.angle() - (TAU / 4)
+	var angle_rads : float = _attack_direction.angle() - (_sweep_angle_rads / 2) + (_sweep_angle_rads * percentage_progress)
 	var direction_from_owner : Vector2 = Vector2.from_angle(angle_rads)
-	rotation = angle_rads + (TAU / 4)
 	
 	var center_offset : Vector2 = Vector2(0, -64) # Horrible hardcoding
 	global_position = _owner.global_position + center_offset + (direction_from_owner * _attack_circle_radius)
+	rotation = angle_rads + (TAU / 4)
